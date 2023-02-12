23c3b2ec87ba5627b7be73355dbc4f918a94f6c9
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-02-12 16:33:16 +0100

    Typo: replaced Dims with Factors

--- RDNecaise/Chapter3/Tests/tests.py
+++ RDNecaise/Chapter3/Tests/tests.py
@@ -5,7 +5,7 @@ from RDNecaise.Chapter3.array import MultiArray
 elements = [2, 15, 45, 13, 78, 40, 12, 52, 91, 86, 59, 25, 33, 41, 6]
 m_array = MultiArray(3, 5)
 m_array.clear(10)
-print("Dims: ", *m_array.factors())
+print("Factors: ", *m_array.factors())
 d1, d2 = 0, 0
 for i, e in enumerate(elements):
     m_array[d1, d2] = e
