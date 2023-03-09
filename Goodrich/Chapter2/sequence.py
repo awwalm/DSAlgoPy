8187a3960d724e6947e7fdd0f24416a2664763a0
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-03-09 16:18:32 +0100

    Partially implemented sequence.py

---
+++ Goodrich/Chapter2/sequence.py
@@ -0,0 +1,22 @@
+"""Demonstration of abstract classes via template method pattern."""
+
+from abc import ABCMeta, abstractmethod
+
+
+class Sequence(metaclass=ABCMeta):
+    """Our own version of ``collections.Sequence`` abstract base class."""
+
+    @abstractmethod
+    def __len__(self):
+        """Return the length of the sequence."""
+
+    @abstractmethod
+    def __getitem__(self, j):
+        """Return the element at index j of the sequence."""
+
+    def __contains__(self, val):
+        """Return ``True`` if val found in the sequence; ``False`` otherwise."""
+        for j in range(len(self)):
+            if self[j] == val:
+                return True
+        return False
