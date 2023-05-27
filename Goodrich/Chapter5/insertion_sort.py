62a9411ecc66a8b126745ff92b29908518f4d450
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-05-27 20:50:51 +0100

    Made comments more intuitive

--- Goodrich/Chapter5/insertion_sort.py
+++ Goodrich/Chapter5/insertion_sort.py
@@ -7,7 +7,7 @@ def insertion_sort(A):
         for k in range(1, len(A)):          # From 1 to n-1.
             cur = A[k]                      # Current element to be inserted.
             j = k                           # Find correct index j for current.
-            while j > 0 and A[j-1] > cur:   # Element A[j-1] must be after current.
-                A[j] = A[j-1]               # Swap places.
+            while j > 0 and A[j-1] > cur:   # If element before current is greater...
+                A[j] = A[j-1]               # swap places.
                 j -= 1
             A[j] = cur                      # Cur is now in the right place.
