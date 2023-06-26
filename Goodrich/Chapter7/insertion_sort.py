4d01cd38f807b3373dd6d1c6a08e56287b788144
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-06-26 11:30:06 +0100

    Added insertion sort algorithm for Positional List

---
+++ Goodrich/Chapter7/insertion_sort.py
@@ -0,0 +1,19 @@
+"""Code Fragment 7.17: Python code for performing insertion-sort on a positional list."""
+from positional_list import PositionalList
+
+
+def insertion_sort(L: PositionalList):
+    """Sort ``PositionalList`` of comparable elements into nondecreasing order."""
+    if len(L) > 1:                          # Otherwise, no need to sort it.
+        marker = L.first()
+        while marker != L.last():
+            pivot = L.after(marker)         # Next item to place.
+            value = pivot.element()
+            if value > marker.element():    # Pivot is already sorted (CAREFUL if value is non-numeric!).
+                marker = pivot              # Pivot becomes new marker.
+            else:                           # Must relocate pivot.
+                walk = marker               # Find leftmost item greater than value.
+                while walk != L.first() and L.before(walk).element() > value:
+                    walk = L.before(walk)
+                L.delete(pivot)
+                L.add_before(walk, value)   # Reinsert value before walk.
