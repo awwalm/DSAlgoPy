9699857cec59a53bb5be73cf878d6500e4422ec9
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-07-10 09:57:43 +0100

    Inheritance bug fix:
    - Internal `_Node` class throws `TypeError` when overloading inherited `SinglyLinkedList._Node` initializer, due to additional `prev` argument.
    
    - Current workaround conforms to original initializer without `prev` argument.
    
    - `self._prev` is explicitly set as a privatized accessible member.

--- Goodrich/Chapter7/Exercises/proper_doubly_linked_list.py
+++ Goodrich/Chapter7/Exercises/proper_doubly_linked_list.py
@@ -6,7 +6,7 @@
 from __future__ import annotations
 from typing import Union
 from overrides import override
-from proper_singly_linked_list import SinglyLinkedList, EmptyError
+from Goodrich.Chapter7.Exercises.proper_singly_linked_list import SinglyLinkedList, EmptyError
 
 
 class DoublyLinkedList(SinglyLinkedList):
@@ -18,15 +18,14 @@ class DoublyLinkedList(SinglyLinkedList):
         - Inherits from the ``SinglyLinkedList``'s internal ``_Node`` class.
         - Extends the ``_Node`` class with a ``_prev`` reference.
         """
-        __slots__ = "_prev"  # Streamline memory usage.
+        __slots__ = "_element", "_prev", "_next"                            # Streamline memory usage.
 
         # noinspection PyShadowsBuiltInName
         def __init__(self,
                      element,
-                     next: Union[DoublyLinkedList._Node, None],
-                     prev: Union[DoublyLinkedList._Node, None]):
-            self._prev = prev
-            super().__init__(element, next)
+                     next: Union[DoublyLinkedList._Node, None]):            # @TODO: Make init accept prev argument.
+            super(DoublyLinkedList._Node, self).__init__(element, next)
+            self._prev = next
 
         @property
         def prev(self):
@@ -40,7 +39,7 @@ class DoublyLinkedList(SinglyLinkedList):
     def __init__(self, head_element):
         """Create a doubly linked list initialized with a head node and a valid element."""
         super(DoublyLinkedList, self).__init__(head_element)
-        self._header: DoublyLinkedList._Node = self._Node(head_element, None, None)
+        self._header: DoublyLinkedList._Node = self._Node(head_element, None)
         self._tail = self._header
 
     def tail(self):
@@ -58,10 +57,12 @@ class DoublyLinkedList(SinglyLinkedList):
         if self.is_empty():
             raise EmptyError("Linked List is empty")
         elif self._size == 1 and guide == self._header:
-            newest = self._Node(e, self._header, None)
+            newest = self._Node(e, self._header)
+            newest.prev = None
             self._header.prev = self._header = newest
         else:
-            newest = self._Node(e, guide, guide.prev)
+            newest = self._Node(e, guide)
+            newest.prev = guide.prev
             guide.prev.next = guide.prev = newest
         self._size += 1
         return newest
@@ -72,10 +73,12 @@ class DoublyLinkedList(SinglyLinkedList):
         if self.is_empty():
             raise EmptyError("Linked List is empty")
         elif guide == self._tail:
-            newest = self._Node(e, self._tail.next, self._tail)
+            newest = self._Node(e, self._tail.next)
+            newest.prev = self._tail
             self._tail.next = self._tail = newest
         else:
-            newest = self._Node(e, guide.next, guide)
+            newest = self._Node(e, guide.next)
+            newest.prev = guide
             guide.next = newest
         self._size += 1
         return newest
