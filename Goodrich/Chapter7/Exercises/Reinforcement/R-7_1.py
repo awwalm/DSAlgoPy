c776171a7c500e2bbf2344cf7be7625b9d167601
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-07-12 16:43:25 +0100

    Moved `print_nodes()` and `populate_nodes()` to dedicated utility module

--- Goodrich/Chapter7/Exercises/Reinforcement/R-7_1.py
+++ Goodrich/Chapter7/Exercises/Reinforcement/R-7_1.py
@@ -1,16 +1,11 @@
 """R-7.1 Give an algorithm for finding the second-to-last node in a singly linked list
 in which the last node is indicated by a next reference of None.
-
-ALGORITHM FindSecondToLast(L: LinkedList):
-    cur = L.head
-    while cur.next != None:
-        return cur if cur.next is L.tail else pass
-        cur = cur.next
 """
-from Goodrich.Chapter7.Exercises.proper_singly_linked_list import SinglyLinkedList
+from Goodrich.Chapter7.Exercises.Utility.utils import *
 
 
-def find_second_to_last(L: SinglyLinkedList):
+# The algorithm.
+def find_second_to_last(L: Union[SinglyLinkedList, DoublyLinkedList]):
     cur = L.head
     while cur is not None and cur.next is not None:
         if cur.next.next is None:
@@ -18,23 +13,7 @@ def find_second_to_last(L: SinglyLinkedList):
         cur = cur.next
 
 
-def print_nodes(L: SinglyLinkedList):
-    cur = L.head
-    while cur is not None:
-        print(cur.element)
-        cur = cur.next
-
-
-def populate_nodes(L: SinglyLinkedList, limit: int):
-    cur = L.head
-    assert cur.element < limit, "limit must be greater than head value"
-    while cur is not None:
-        L.insert_after(cur.element + 1, cur)
-        cur = cur.next
-        if cur.element == limit:
-            break
-
-
+# Create a new singly linked list with a node with element 1.
 l = SinglyLinkedList(1)
 
 # Populate Linked List with values 1 to 10.
@@ -46,13 +25,3 @@ print_nodes(l)
 # And now for my final trick...print 9!
 n = find_second_to_last(l)
 print(n.element)
-
-# One more trick: insert 2.5 before 3, and 9.5 after 9.
-curnode = l.head
-while curnode is not None:
-    if curnode.element == 3:
-        l.insert_before(2.5, curnode)
-    if curnode.element == 9:
-        l.insert_after(9.5, curnode)
-    curnode = curnode.next
-print_nodes(l)
