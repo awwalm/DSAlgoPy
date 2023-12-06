e3348ada94b9c05b9e58daccb573cdc6d877cdf4
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-12-06 18:55:09 +0300

    Added `UnsortedMap` test cases for exercise R-10.5

---
+++ Goodrich/Chapter10/Exercises/Reinforcement/test_r_10_5.py
@@ -0,0 +1,48 @@
+import unittest
+from Goodrich.Chapter10.Exercises.Reinforcement.R_10_5 import UnsortedTableMap
+unittest.TestLoader.sortTestMethodsUsing = None  # Stop f#$%ing running tests in sporadic order
+
+
+class TestPositionalUnsortedMap(unittest.TestCase):
+    def setUp(self):
+        self.s = UnsortedTableMap()
+
+    def test1Stringified(self):
+        print(f"Unsorted map created: {self.s}")
+        print(self.s)
+
+    def test2SetItem(self):
+        self.s["One"] = 1
+        print(self.s)
+        self.assertEqual(1, self.s["One"])
+
+    def test3BulkSetItem(self):
+        self.s["One"] = 1
+        self.s["Two"] = 2
+        self.s["Three"] = 3
+        self.s["Four"] = 4
+        self.s["Five"] = 5
+        self.assertEqual(len(self.s), 5)
+
+    def test4DeleteItem(self):
+        self.s["One"] = 1
+        self.s["Two"] = 2
+        self.s["Three"] = 3
+        self.s["Four"] = 4
+        self.s["Five"] = 5
+        print(f"before delete: {self.s}")
+        del self.s["One"]
+        print(f"after delete: {self.s}")
+        self.assertEqual(len(self.s), 4)
+
+    def test5StringRep(self):
+        self.s["One"] = 1
+        self.s["Two"] = 2
+        self.s["Three"] = 3
+        self.s["Four"] = 4
+        self.s["Five"] = 5
+        print(self.s)
+
+
+if __name__ == '__main__':
+    unittest.main()
