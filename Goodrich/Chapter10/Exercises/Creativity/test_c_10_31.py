73bb3ec44c7b7dcc0cacd0d774f1da26b96e3134
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-12-11 18:20:24 +0300

    Removed unnecessary tests

--- Goodrich/Chapter10/Exercises/Creativity/test_c_10_31.py
+++ Goodrich/Chapter10/Exercises/Creativity/test_c_10_31.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from Goodrich.Chapter10.Exercises.Creativity.C_10_31 import (
     SOE, sieve
 )
@@ -15,6 +16,14 @@ class MyTestCase(unittest.TestCase):
     def test_sieve(self):
         print(f"\nTest for range [{self._m}, {2*self._m}):")
         print(*sieve(self._m), sep='\n')
+
+    def test_sieve_performance(self):
+        print("Optimized:")
+        t1 = time.time()
+        for i,j in zip([n for n in range(self._m + 1, 2*self._m + 1)], sieve(self._m)):
+            print(f"{i} is {'prime' if j else 'not prime'}")
+        t2 = time.time()
+        print(f"Speed: {t2 - t1:.8f}")
 
 
 if __name__ == '__main__':
