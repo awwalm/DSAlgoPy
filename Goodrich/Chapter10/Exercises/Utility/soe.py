9d29a0564fd48daa973b49d5ad9367094c309c69
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-12-13 19:17:45 +0300

    Added a definitive and optimal SOE implementation

--- Goodrich/Chapter10/Exercises/Utility/soe.py
+++ Goodrich/Chapter10/Exercises/Utility/soe.py
@@ -9,6 +9,37 @@ def optimal_sieve(m: int):
             BA[j] = False
     return [i for i,j in zip(range(2, m + 1), BA[2:]) if j is True]
 
+def correct_sieve(m: int):
+    BA = [True] * m
+    rtm = int(m**(1/2)) + 1
+    p = []
+    for i in range(2, rtm):
+        if BA[i]:           # Starts from no. 2 which is prime
+            p.append(i)
+            f = i
+            while f < len(BA):
+                BA[f] = False
+                f += i
+    for i in range(2, len(BA)):
+        if BA[i]: p.append(i)
+    return p
+
+
+def SOE2(m: int):
+    """Standard Sieve of Eratosthenes algorithm that finds primes from 0 till m."""
+    BA = [True] * m
+    for i, k in zip(range(2, (m//2) + 1), range(len(BA)//2)):
+        if BA[k] is False: continue
+        for j in range(2, i):
+            if i % j == 0:
+                BA[k] = False
+                f = k + j
+                while f < len(BA):
+                    BA[f] = False
+                    f += j
+                break
+    return [i for i,j in zip(range(2, m + 1), BA) if j is True]
+
 def primes_sieve2(limit):
     """From Stackoverflow: https://stackoverflow.com/a/3941967/13488161"""
     a = [True] * limit                          # Initialize the primality list
