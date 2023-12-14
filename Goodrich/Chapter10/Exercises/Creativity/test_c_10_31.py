4d8cd1c0695e87f040a1f5e46f32a66271f35fb4
Awwal M <67423428+awwalm@users.noreply.github.com>
2023-12-14 23:41:11 +0300

    Updated imports

--- Goodrich/Chapter10/Exercises/Creativity/test_c_10_31.py
+++ Goodrich/Chapter10/Exercises/Creativity/test_c_10_31.py
@@ -1,29 +1,27 @@
 import unittest
 import time
-from Goodrich.Chapter10.Exercises.Creativity.C_10_31 import (
-    SOE, sieve
-)
+from Goodrich.Chapter10.Exercises.Creativity.C_10_31 import soe, sieve
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self, m = 30): #random.randrange(1,23)):
+    def setUp(self, m = 40): #random.randrange(1,23)):
         self._m = m
 
     def test_soe(self):
         print(f"\nTest for range [0, {self._m}):")
-        print(*SOE(self._m), sep='\n')
+        print(*soe(self._m), sep='\n')
 
     def test_sieve(self):
         print(f"\nTest for range [{self._m}, {2*self._m}):")
         print(*sieve(self._m), sep='\n')
 
-    def test_sieve_performance(self):
+    """def test_sieve_performance(self):
         print("Optimized:")
         t1 = time.time()
         for i,j in zip([n for n in range(self._m + 1, 2*self._m + 1)], sieve(self._m)):
             print(f"{i} is {'prime' if j else 'not prime'}")
         t2 = time.time()
-        print(f"Speed: {t2 - t1:.8f}")
+        print(f"Speed: {t2 - t1:.8f}")"""
 
 
 if __name__ == '__main__':
