3b4b93aaf90ad0dd66d438f98ddf06153b2f0fb4
Awwal M <67423428+awwalm@users.noreply.github.com>
2024-02-01 22:39:26 +0300

    Quick implementations of sorting and search algorithms

---
+++ Goodrich/Chapter12/Tests/playgrounds.py
@@ -0,0 +1,81 @@
+"""Sample snippets and short tests."""
+
+def bin_search(val, seq, start, end):
+    if start == len(seq) or end == 0:                           # Range exceeded
+        return False
+    else:
+        mid = (start + end) // 2
+        if val == seq[mid]:
+            return True
+        elif val < seq[mid]:
+            return bin_search(val, seq, start, mid-1)
+        else:                                                   # val > seq[mid]
+            return bin_search(val, seq, mid+1, end)
+
+s = [-15,2,3,4,5,6,7,8]
+print(f"-9 in {s}: {bin_search(-9, s, 0, len(s)-1)}")      # False
+print(f"7 in {s}: {bin_search(7, s, 0, len(s)-1)}")    # True
+
+def insertion_sort(seq):
+    if 0 <= len(seq) <= 1: return
+    for i in range(1, len(seq)):
+        j, k = i-1, i
+        while j > -1:
+            if seq[k] < seq[j]:
+                seq[k], seq[j] = seq[j], seq[k]
+            j -= 1
+            k -= 1
+
+s2 = [4,1,5,7,4,9,-3,3,-15,15,0,25,18]
+insertion_sort(s2)
+print(s2)
+
+def selection_sort_too_many_rewrites(seq):
+    if 0 <= len(seq) <= 1: return
+    for i in range(len(seq)):
+        for j in range(i+1, len(seq)):
+            if seq[j] < seq[i]:
+                seq[i], seq[j] = seq[j], seq[i]
+
+def selection_sort(seq):
+    if 0 <= len(seq) <= 1: return
+    for i in range(len(seq)):
+        minval, searchpos = seq[i], i
+        for j in range(i+1, len(seq)):
+            if seq[j] < minval:
+                minval, searchpos = seq[j], j
+        seq[i], seq[searchpos] = minval, seq[i]
+
+s3 = [4,1,5,7,4,9,-3,3,-15,15,0,25,18]
+selection_sort(s3)
+print(s3)
+
+def bubble_sort(seq):
+    if 0 <= len(seq) <= 1: return
+    for i in range(len(seq)):
+        for j in range(1, len(seq)):
+            if seq[j] < seq[j-1]:
+                seq[j], seq[j-1] = seq[j-1], seq[j]
+
+s4 = [4,1,5,7,4,9,-3,3,-15,15,0,25,18]
+bubble_sort(s4)
+print(s4)
+
+# @FIXME: Not completed
+def merge_sort(seq):
+    if 0 <= len(seq) <= 1: return seq
+    if len(seq) > 2:
+        print(merge_sort(seq[:len(seq)//2]) + merge_sort(seq[len(seq)//2:]))
+    elif len(seq) == 2:
+        if seq[1] < seq[0]:
+            seq[0], seq[1] = seq[1], seq[0]
+    #print(seq)
+    return seq
+
+s5 = [4,1,5,7,4,9,-3,3,-15,15,0,25,18]
+s5 = merge_sort(s5)
+print(s5)
+
+
+
+
