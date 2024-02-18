41bb599f75aaf6c9892c37f6345c92941332399d
Awwal M <67423428+awwalm@users.noreply.github.com>
2024-02-18 19:17:51 +0300

    Added a recursive string reversal algorithm

--- Goodrich/Chapter4/Exercises/Projects/tests.py
+++ Goodrich/Chapter4/Exercises/Projects/tests.py
@@ -1,4 +1,17 @@
+def test_string_formatting_justification():
+    for i, j, k in zip([i for i in range(90, 110)], [i for i in range(90, 110)], [i for i in range(90, 110)]):
+        print("%+6s %+6s %+6s" % (i, j, k))
 
-for i, j, k in zip([i for i in range(90, 110)], [i for i in range(90, 110)], [i for i in range(90, 110)]):
-    print("%+6s %+6s %+6s" % (i, j, k))
+def reverse_string(s):
+    sout = []
+    recrusive_reversal(s, 0, sout)
+    return "".join(sout)
 
+def recrusive_reversal(s: str, ndx: int, output: list):
+    if ndx < len(s):
+        recrusive_reversal(s, ndx+1, output)
+        output.append(s[ndx])
+
+
+for w in ("racecar", "madam", "radar", "olayinka", "america", "python"):
+    print(reverse_string(w))
