1c75cd427b6820a994af117735288ca8d37c2e24
Awwal M <67423428+awwalm@users.noreply.github.com>
2024-04-27 19:13:15 +0300

    Finalized DP LCS solution

--- Goodrich/Chapter13/Utilities/LCS/lcs_dp.py
+++ Goodrich/Chapter13/Utilities/LCS/lcs_dp.py
@@ -1,15 +1,13 @@
-"""
-Longest Common Subsequence solution using classic Dynamic Programming.
-"""
+"""Longest Common Subsequence solution using classic Dynamic Programming."""
 import time
 
 
 def BUILD_LCS(A: str, B: str, L: dict):
     n, m = len(A), len(B)
-    for row in range(n+1): L[(row, 0)] = 0              # Fill first column with 0 to allow initialization
-    for col in range(m+1): L[(0, col)] = 0              # Fill first row with 0 to allow initialization
-    for k in range(1, m+1):                             # Column iteration [we calculate columns first]
-        for j in range(1, n+1):                         # Row iteration
+    for row in range(n+1): L[(row, 0)] = 0      # Initialize first column cells to 0
+    for col in range(m+1): L[(0, col)] = 0      # Initialize first row cells to 0
+    for k in range(1, m+1):                     # Fill columns first (left to right)
+        for j in range(1, n+1):                 # Row iteration
             if A[j-1] == B[k-1]:
                 L[(j,k)] = 1 + L[(j-1,k-1)]
             else:
@@ -20,31 +18,33 @@ def BUILD_LCS(A: str, B: str, L: dict):
 def GET_LCS(A: str, B: str, L: dict):
     S = []
     n, m = len(A), len(B)
-    while (n > 0) and (m > 0):
-        if (L[(n, m)] > L[(n, m-1)]) or (L[(n, m)] != L[(n, m-1)]):
-            S.append(m-1)
+    while L[(n,m)] > 0:
+        if A[n-1] == B[m-1]:
+            S.append(A[n-1])
             n -= 1; m -= 1
+        elif L[(n-1, m)] >= L[(n, m-1)]:
+            n -= 1
         else:
             m -= 1
-    return list(map(lambda f: B[f], reversed(S)))
-
+    return str().join(reversed(S))
 
 
 if __name__ == "__main__":
     pairs = [
-        ("ABCBDAB", "BDCAB"),  # BDAB (n=4)
-        ("GTTCCTAATA", "CGATAATTGAGA"),  # GTTTAA (6)
-        ("GTTCCTAAT", "CGATAATTGAG"),  # GTTTA (5)
-        ("12345", "23415"),  # 2345 (4)
-        ("123", "1323"),  # 123 (3)
-        ("126548", "216544"),  # 2654 (4)
-        ("AGGTAB", "GXTXAYB"),  # GTAB (4)
-        ("BD", "ABCD"),  # BD (2)
-        ("ABCDGH", "AEDFHR"),  # ADH (3)
-        ("ABCDE", "ACE"),  # ACE (3)
+        ("ABCBDAB", "BDCAB"),               # BDAB (n=4)
+        ("GTTCCTAATA", "CGATAATTGAGA"),     # GTTTAA (6)
+        ("GTTCCTAAT", "CGATAATTGAG"),       # GTTTA (5)
+        ("12345", "23415"),                 # 2345 (4)
+        ("123", "1323"),                    # 123 (3)
+        ("126548", "216544"),               # 2654 (4)
+        ("AGGTAB", "GXTXAYB"),              # GTAB (4)
+        ("BD", "ABCD"),                     # BD (2)
+        ("ABCDGH", "AEDFHR"),               # ADH (3)
+        ("ABCDE", "ACE"),                   # ACE (3)
         ("hofubmnylkra", "pqhgxgdofcvmr"),  # hofmr (5)
-        ("oxcpqrsvwf", "shmtulqrypy"),  # qr (2)"
-        ("ABC", "DEF"),  # ∅ (0)
+        ("oxcpqrsvwf", "shmtulqrypy"),      # qr (2)"
+        ("ABC", "DEF"),                     # ∅ (0)
+        ("stone", "longest"),               # one (3)
     ]
 
     for p in pairs:
@@ -58,9 +58,9 @@ if __name__ == "__main__":
         print(f"LCS len = {tab[(len(p[0]), len(p[1]))]}")
         print(f"Time taken [LCS DP] = {t2-t1:.10f}s")
 
-        # DP Table printing
-        for c in range(len(p[1]) + 1):
+        # DP table printing
+        for r in range(len(p[0]) + 1):
             ri = []
-            for r in range(len(p[0]) + 1):
-                ri.append(tab[(r, c)])
+            for c in range(len(p[1]) + 1):
+                ri.append(tab[(r,c)])
             print(ri)
