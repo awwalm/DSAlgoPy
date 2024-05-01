1a3e6a825600423ce8e0cd1452d05e46fd563eac
Awwal M <67423428+awwalm@users.noreply.github.com>
2024-05-01 23:51:34 +0300

    Fixed premature variable usage

--- Goodrich/Chapter13/PatternMatching/BoyerMoore/fast_good_suffix.py
+++ Goodrich/Chapter13/PatternMatching/BoyerMoore/fast_good_suffix.py
@@ -17,6 +17,7 @@ def suffixes(x):
     suff = [0] * m
     mMinus1 = m - 1
     g = mMinus1
+    f = g - 1
     for i in range(g - 1, -1, -1):
         if i > g and (suff[i + mMinus1 - f] != i - g):
             suff[i] = min(i - g, suff[i + mMinus1 - f])
@@ -46,3 +47,24 @@ def clPreBmGs(x):
     for i in range(mMinus2 + 1):
         bmGs[mMinus1 - suff[i]] = mMinus1 - i
     return bmGs
+
+
+if __name__ == "__main__":
+    # Sample patterns
+    patterns = ("WOWWOW",       # 25333
+                "BAOBAB",       # 25555
+                "ABCBAB",       # 24444
+                "ANPANMAN",     # 8366666
+                "10000",        # 3215
+                "01010",        # 4422
+                "DRIDI",        # 2555
+                "TCCTATTCTT",
+                )
+
+    # Test GST construction for collection of patterns
+    for pattern in patterns:
+        bmGs_table = clPreBmGs(pattern)
+        print(f"\nGood-Suffix Table for {pattern}:")
+        print("Index\tShift Value")
+        for svi, shift_value in enumerate(bmGs_table):
+            print(f"{svi}\t\t{shift_value}")
