17d6a1ef9f193ec25afe749a994ab7912b84c965
Awwal M <67423428+awwalm@users.noreply.github.com>
2024-07-07 19:24:08 +0300

    Favored reusability - duplicate code removed

--- SolutionsChallenges/Graphs/floyd_warshall.py
+++ SolutionsChallenges/Graphs/floyd_warshall.py
@@ -1,50 +1,24 @@
+"""Chained BFS implementation of Floyd-Warshall transitive closure algorithm."""
+
 import copy
 from collections import OrderedDict
+from Goodrich.Chapter14.Tests.init_graph import *
 from Goodrich.Chapter14.graph import Graph
 Vertex = Graph.Vertex
 Edge = Graph.Edge
 
-def init_directed_graph():
-    G = Graph(directed=True)
-    v1 = G.insert_vertex(1)
-    v2 = G.insert_vertex(2)
-    v3 = G.insert_vertex(3)
-    v4 = G.insert_vertex(4)
-    G.insert_edge(v1, v2)
-    G.insert_edge(v1, v3)
-    G.insert_edge(v3, v2)
-    G.insert_edge(v3, v4)
-    G.insert_edge(v4, v3)
-    return G
-
-
-def init_undirected_graph():
-    G = Graph()
-    A = G.insert_vertex("A")
-    B = G.insert_vertex("B")
-    C = G.insert_vertex("C")
-    D = G.insert_vertex("D")
-    E = G.insert_vertex("E")
-    G.insert_edge(A, C)
-    G.insert_edge(B, C)
-    G.insert_edge(B, D)
-    G.insert_edge(C, D)
-    G.insert_edge(D, E)
-    return G
-
 
 def transitive_bfs(
-        G: Graph,           # Adjacency Map Graph ADT
-        origin: Vertex,     # Original vertex to connect with target t
-        start: Vertex,      # Starting vetex of graph G
-        target: Vertex,     # Target vertex being searched for
-        discovery: OrderedDict[Vertex, Edge],   # Discovery records
-        found: bool):       # Boolean that is true when t is found
-    """BFS traversal that finds target from origin,
-    then connects an edge from origin to target."""
+        G:          Graph,                      # Adjacency Map Graph ADT
+        origin:     Vertex,                     # Original vertex to connect with target t
+        start:      Vertex,                     # Starting vetex of graph G
+        target:     Vertex,                     # Target vertex being searched for
+        discovery:  OrderedDict[Vertex, Edge],  # Discovery records
+        found:      bool):                      # Boolean that is true when t is found
+    """BFS traversal that finds target from origin, then connects an edge from origin to target."""
 
-    outgoing_edges = list(G.incident_edges(
-        start, outgoing=True if G.is_directed() else False))
+    outgoing_edges = G.incident_edges(
+        start, outgoing=True if G.is_directed() else False)
     outgoing_vertices = []
     for e in outgoing_edges:
         v = e.opposite(start)
@@ -74,8 +48,8 @@ def floyd_warshall(G: Graph):
 
 
 if __name__ == "__main__":
-    g1 = init_directed_graph()
-    g2 = init_undirected_graph()
+    g1 = init_directed_graph1()[0]
+    g2 = init_undirected_graph1()[0]
     c1 = floyd_warshall(g1)
     c2 = floyd_warshall(g2)
     edges1 = c1.edges()
