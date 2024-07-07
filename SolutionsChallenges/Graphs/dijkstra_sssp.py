6185dd603dc5176e0ccfe6d5df0c1b11d1ec143c
Awwal M <67423428+awwalm@users.noreply.github.com>
2024-07-07 19:25:51 +0300

    `distance` now maps `Vertex` to `DistanceEntry` instances

--- SolutionsChallenges/Graphs/dijkstra_sssp.py
+++ SolutionsChallenges/Graphs/dijkstra_sssp.py
@@ -1,50 +1,54 @@
 """Dijkstra's Single Source Shortest Path Algorithm."""
 
 import math
-from typing import Dict, List
+from dataclasses import dataclass
+from typing import Dict
 from Goodrich.Chapter13.GreedyTextCompression.min_heap import MinHeap
 from Goodrich.Chapter14.Tests.init_graph import *
 from Goodrich.Chapter14.graph import Graph
 Vertex = Graph.Vertex
 Edge = Graph.Edge
 Int = int
+Float = float
 Bool = bool
 
 
 def dijkstra(G: Graph, s: Vertex):
     """Return the record of shortest paths from s to all other vertices in G."""
 
-    distance: Dict[Vertex, List[Int and (Vertex|None) and Bool]]  = dict()
-    """Tri-column distance record: Distance, Predecessor, Visted?"""
+    @dataclass
+    class DistanceEntry: cost: Int|Float; predecessor: Vertex|None; visited: Bool
+    distance: Dict[Vertex, DistanceEntry] = dict()
+    """Maps each vertex in G to distance properties (cost, predecessor, visited)."""
 
     unvisited = MinHeap()
-    """Priority Queue for ranking the distance of a vertex (key) and vertex itself (value)."""
+    """Priority Queue for ranking the distance of a vertex (key) and said vertex (value)."""
 
     # Insert starting vertex in unvisited heap and declare distance properties
     unvisited.insert(k=0, v=s)
-    distance[s] = [0, None, False]
+    distance[s] = DistanceEntry(cost=0, predecessor=None, visited=False)
 
     # Declare distance records for other nodes
     for vert in G.vertices():
         if vert is not s:
-            distance[vert] = [math.inf, None, False]
+            distance[vert] = DistanceEntry(math.inf, None, False)
 
     # Begin iterative relaxation and distance updating process
     cur_vert = s
     while len(unvisited) > 0:
-        if not distance[cur_vert][2]:                   # If node is yet to be visited...
-            for edge in G.incident_edges(cur_vert):     # For each adjacent neighbor...
+        if not distance[cur_vert].visited:                      # If node is yet to be visited...
+            for edge in G.incident_edges(cur_vert):             # For each adjacent neighbor...
                 weight = edge.element()
                 neighbor = edge.opposite(cur_vert)
-                if distance[cur_vert][0] + weight < distance[neighbor][0]:
-                    distance[neighbor][0] = \
-                        distance[cur_vert][0] + weight  # Relax/update distance
-                    distance[neighbor][1] = cur_vert    # Update predecessor
-                if not distance[neighbor][2]:           # If vertex has not been visited...
-                    unvisited.insert(                   # Insert into univisted heap
-                        k=distance[neighbor][0], v=neighbor)
-        distance[cur_vert][2] = True                    # Mark as visited
-        cur_vert = unvisited.remove_min()[1]            # Choose next minimum
+                if distance[cur_vert].cost + weight < distance[neighbor].cost:
+                    distance[neighbor].cost = (                 # Relax/update distance
+                            distance[cur_vert].cost + weight)
+                    distance[neighbor].predecessor = cur_vert   # Update predecessor
+                if not distance[neighbor].visited:              # If vertex has not been visited...
+                    unvisited.insert(                           # Insert into univisted heap
+                        k=distance[neighbor].cost, v=neighbor)
+        distance[cur_vert].visited = True                       # Mark as visited
+        cur_vert = unvisited.remove_min()[1]                    # Choose next minimum
 
     return distance
 
@@ -58,6 +62,6 @@ if __name__ == "__main__":
         for k in paths:
             path = paths[k]
             print(f"{k.element()}\t|\t"
-                  f"{path[0]}\t|\t"
-                  f"{path[1].element() if path[1] else '$'}\t|\t"
-                  f"{path[2]}\t|\t")
+                  f"{path.cost}\t|\t"
+                  f"{path.predecessor.element() if path.predecessor else '$'}\t|\t"
+                  f"{path.visited}\t|\t")
