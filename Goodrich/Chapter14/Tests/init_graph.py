fc03cc7fc73bc329ec719007a40e136f67a5279d
Awwal M <67423428+awwalm@users.noreply.github.com>
2024-08-20 00:03:42 +0300

    Tree-like undirected graph initializers

--- Goodrich/Chapter14/Tests/init_graph.py
+++ Goodrich/Chapter14/Tests/init_graph.py
@@ -165,3 +165,72 @@ def init_complete_graph(n=random.randint(4,10), weight_range=(1,100)):
 
     return G
 
+# Create an undirected graph acyclic and connected (tree)
+def init_bin_tree_graph():
+    """For testing modified Uniform Cost Search on a tree."""
+    G = Graph(directed=False)
+    s = G.insert_vertex("S")
+    a = G.insert_vertex("A")
+    b = G.insert_vertex("B")
+    c = G.insert_vertex("C")
+    d = G.insert_vertex("D")
+    e = G.insert_vertex("E")
+    f = G.insert_vertex("F")
+    g1 = G.insert_vertex("G1")
+    g2 = G.insert_vertex("G2")
+    g3 = G.insert_vertex("G3")
+
+    # Forming a tree structure
+    G.insert_edge(s, a)  # S - A
+    G.insert_edge(s, b)  # S - B
+    G.insert_edge(b, c)  # B - C
+    G.insert_edge(c, g1)  # C - G1
+    G.insert_edge(c, d)  # C - D
+    G.insert_edge(d, e)  # D - E
+    G.insert_edge(e, g2)  # E - G2
+    G.insert_edge(e, f)  # E - F
+    G.insert_edge(f, g3)  # F - G3
+
+    # return G, s, {g1, g2, g3}
+    return G, s, g3
+
+
+def init_tree_graph():
+    """Create a non-binary tree structure for testing."""
+    G = Graph(directed=False)
+
+    # Insert vertices
+    s = G.insert_vertex("S")
+    a = G.insert_vertex("A")
+    b = G.insert_vertex("B")
+    c = G.insert_vertex("C")
+    d = G.insert_vertex("D")
+    e = G.insert_vertex("E")
+    f = G.insert_vertex("F")
+    g = G.insert_vertex("G")
+    h = G.insert_vertex("H")
+    i = G.insert_vertex("I")
+    j = G.insert_vertex("J")
+    k = G.insert_vertex("K")
+    l = G.insert_vertex("L")
+    m = G.insert_vertex("M")
+
+    # Insert edges (tree structure)
+    G.insert_edge(s, a)  # S - A
+    G.insert_edge(s, b)  # S - B
+    G.insert_edge(s, c)  # S - C
+    G.insert_edge(a, d)  # A - D
+    G.insert_edge(a, e)  # A - E
+    G.insert_edge(e, i)  # E - I
+    G.insert_edge(i, k)  # I - K
+    G.insert_edge(i, l)  # I - L
+    G.insert_edge(c, f)  # C - F
+    G.insert_edge(c, g)  # C - G
+    G.insert_edge(c, h)  # C - H
+    G.insert_edge(h, j)  # H - J
+    G.insert_edge(j, m)  # J - M
+
+    # return G, s, {d, k, m}  # Mark some nodes as goals (you can change this)
+    return G, k, f
+
+
